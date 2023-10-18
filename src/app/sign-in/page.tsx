91908906923d572c6ffd5b9919189e29b26d0e2f
Vinicius Peres Barizon <vinicius@com2.com.br>
2023-10-18 11:00:40 -0300

    chore: login refactor

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -12,32 +12,11 @@ export default function SignIn() {
 
   const router = useRouter();
 
-  const handleSubmit = async (e: any) => {
-    e.preventDefault();
-
-    try {
-      const result = await signIn("credentials", {
-        email: email.current,
-        password: password.current,
-        redirect: false,
-      });
-
-      if (result?.error) {
-        setError("Usuário ou senha inválidos");
-        return;
-      }
-      router.replace("/");
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
   return (
     <>
       <div className="flex items-center justify-center h-screen">
         <div className="mx-auto w-2/5 text-center">
           <form
-            onSubmit={handleSubmit}
             className="bg-zinc-200 rounded-lg p-14 text-center"
           >
             <h1>Login</h1>
@@ -58,8 +37,19 @@ export default function SignIn() {
                 <input type="checkbox" className="mr-2" /> Lembrar de mim
               </label>
             </div>
-            <button className="bg-sky-300 hover:bg-sky-400 rounded-lg p-2 cursor-pointer border w-full">
-              Entrar
+            <button 
+              className="bg-sky-300 hover:bg-sky-400 rounded-lg p-2 cursor-pointer border w-full"
+              onClick={async (e) => {
+                e.preventDefault();
+                await signIn('credentials', { 
+                  email: email.current,
+                  password: password.current,
+                  redirect: false
+                });
+                router.replace("/");
+              }}
+            >
+              Login
             </button>
             {error && (
               <div className="bg-red-500 text-white w-fit text-sm py-1 mt-5 px-3 rounded-md">
