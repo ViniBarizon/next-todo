2515b9161227fdb6d04d743f8e6e6ffebea69a17
Vinicius Peres Barizon <vinicius@com2.com.br>
2023-10-18 10:09:11 -0300

    feat: redirect to sign-in from root url when user not authenticated.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,19 @@
 'use client';
 
 import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+
 
 export default function Home() {
-const teste = useSession();
+const authStatus = useSession().status;
+
+const router = useRouter();
   return (
     <>
-      
+      {authStatus === "authenticated" && <h1>teste</h1>}
+      {authStatus !== "authenticated" && 
+        router.replace("/sign-in")
+      }      
     </>
   )
 }
